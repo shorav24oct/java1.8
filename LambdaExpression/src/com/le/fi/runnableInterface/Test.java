fc9b7a59713e814b7f972f602c9ea58409b3082f
shoravs <sharmasaurav142@gmail.com>
2022-07-15 07:13:45 +0530

    lambda expression for runnable run method

---
+++ LambdaExpression/src/com/le/fi/runnableInterface/Test.java
@@ -0,0 +1,34 @@
+package com.le.fi.runnableInterface;
+
+class MyRunnable implements Runnable {
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++)
+			System.out.println("Child Thread");
+	}
+
+}
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Runnable myRunnable = new MyRunnable();
+		
+		/*
+		 * Lambda Expression for Implementation of run()
+		 */
+		Runnable r = () -> {
+			for (int i = 0; i < 10; i++)
+				System.out.println("Lambda Thread");
+		};
+
+		Thread thread = new Thread(r);
+		thread.start();
+
+		for (int i = 0; i < 10; i++)
+			System.out.println("Main Thread");
+	}
+
+}
